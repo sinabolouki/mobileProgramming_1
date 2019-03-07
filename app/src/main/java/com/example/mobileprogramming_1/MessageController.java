98e01444fbf32652c4aa836e0a5b0c688c0343a4
Emad Salehi <emad.s1178@yahoo.com>
2019-03-07 22:18:38 +0330

    after threads works in messageContoller added.

--- app/src/main/java/com/example/mobileprogramming_1/MessageController.java
+++ app/src/main/java/com/example/mobileprogramming_1/MessageController.java
@@ -1,4 +1,33 @@
 package com.example.mobileprogramming_1;
 
+import android.os.storage.StorageManager;
+
+import java.util.ArrayList;
+
 public class MessageController {
+    ArrayList <Integer> numbers = new ArrayList<>();
+    StorageManager storageManager = new StorageManager();
+    ConnectionManager connectionManager = new ConnectionManager();
+    NotificationCenter notificationCenter = NotificationCenter.getInstance();
+
+    public void fetch(boolean fromCache) {
+        ArrayList<Integer> output = new ArrayList<>();
+        if(fromCache) {
+
+        } else {
+            
+        }
+
+        synchronized (this) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        numbers.addAll(output);
+        notificationCenter.setDataLoaded(true);
+        if(!fromCache)
+            storageManager.save(numbers.get(numbers.size() - 1));
+    }
 }
