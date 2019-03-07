4771e8afd0bd886f6ab987375718add73b79f140
Emad Salehi <emad.s1178@yahoo.com>
2019-03-07 23:55:30 +0330

    linearLayout fielded!!

--- app/src/main/java/com/example/mobileprogramming_1/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming_1/MainActivity.java
@@ -6,15 +6,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import java.util.concurrent.ExecutorService;
+
 public class MainActivity extends AppCompatActivity {
+    final LinearLayout linearLayout = findViewById(R.id.linearLayout);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
-        final LinearLayout linearLayout = findViewById(R.id.linearLayout);
 
         final Button clearButton = findViewById(R.id.clear_button);
         clearButton.setOnClickListener(new View.OnClickListener() {
@@ -24,4 +25,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
