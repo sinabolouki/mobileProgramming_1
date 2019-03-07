04bd63ab0d1e9a08a31bfebe4176e8120dd8cfec
SinaBolouki <sina.bolouki@gmail.com>
2019-03-07 14:32:58 +0330

    NotificationCenter added

---
+++ app/src/main/java/com/example/mobileprogramming_1/NotificationCenter.java
@@ -0,0 +1,40 @@
+package com.example.mobileprogramming_1;
+
+import android.util.SparseArray;
+
+import java.util.ArrayList;
+
+public class NotificationCenter {
+    private ArrayList<Observer> observers = new ArrayList<>();
+    private boolean dataLoaded = false;
+    private static NotificationCenter notificationCenterInstance = null;
+
+    private NotificationCenter () {
+
+    }
+
+    public NotificationCenter getInstance() {
+        if (notificationCenterInstance == null)
+            notificationCenterInstance = new NotificationCenter();
+        return notificationCenterInstance;
+    }
+    public interface Observer {
+        public void update();
+    }
+
+    public void setDataLoaded (boolean dataLoaded) {
+        this.dataLoaded = dataLoaded;
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+
+    public void registerObserver (Observer observer)  {
+        observers.add(observer);
+    }
+
+    public void unregisterObserver (Observer observer) {
+        observers.remove(observer);
+    }
+
+}
