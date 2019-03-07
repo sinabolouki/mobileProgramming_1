716c8dcac840fb03c25ec3a6ac6357b4a129c72b
ashkanlarni <ash.larni@gmail.com>
2019-03-07 23:46:59 +0330

    clear button done

--- app/src/main/java/com/example/mobileprogramming_1/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming_1/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.mobileprogramming_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        final LinearLayout linearLayout = findViewById(R.id.linearLayout);
+
+        final Button clearButton = findViewById(R.id.clear_button);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linearLayout.removeAllViews();
+            }
+        });
     }
 }
