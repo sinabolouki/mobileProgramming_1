963d4c7acfe554fd7743b562de19bc7d32b8522b
Emad Salehi <emad.s1178@yahoo.com>
2019-03-08 00:01:47 +0330

    get button connected.

--- app/src/main/java/com/example/mobileprogramming_1/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming_1/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void getAction() {
+    public void getAction(View v) {
         messageController.fetch(false);
     }
 }
